238ce72ff680b64584c559574a33d6b73e603f7f
Bamidele Isaac <bamideleisaac11@gmail.com>
2023-11-17 17:10:51 +0100

    allow cross origin

--- packages/robo-advisor-backend/config/allowedOrigins.js
+++ packages/robo-advisor-backend/config/allowedOrigins.js
@@ -1,6 +1,4 @@
 const allowedOrigins = [
-  'http://localhost:3000',
-  'http://localhost:4200',
   'https://robo-advisor-robo-advisor-frontend-hiziknewton.vercel.app/',
   'robo-advisor-robo-advisor-frontend.vercel.app',
 ];
--- packages/robo-advisor-backend/config/corsOptions.js
+++ packages/robo-advisor-backend/config/corsOptions.js
@@ -1,9 +1,8 @@
 const allowedOrigins = require('./allowedOrigins');
 
-const corsOptions = {
+/* const corsOptions = {
   origin: (origin, callback) => {
-    console.log(origin, 'origin');
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    if (allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
@@ -11,6 +10,16 @@ const corsOptions = {
   },
   credentials: true,
   optionsSuccessStatus: 200,
+}; */
+
+var corsOption = function (req, callback) {
+  var corsOptions;
+  if (allowedOrigins.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
 };
 
 module.exports = corsOptions;
