6e4dd4469a2ff14ddb9078d401ac9b75963d0a41
Bamidele Isaac <bamideleisaac11@gmail.com>
2023-11-17 12:53:59 +0100

    production ready

--- packages/robo-advisor-backend/config/dbConnect.js
+++ packages/robo-advisor-backend/config/dbConnect.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const connectDB = async () => {
   try {
-    const env = dotenv.config({ path: `.env.local`, override: true });
-    mongoose.connect(env.parsed.LOCAL_DB).then(() => {
+    const env = dotenv.config({ path: `.env.prod`, override: true });
+    mongoose.connect(env.parsed.MONGODB_CONNECTION_STRING).then(() => {
       const db = mongoose.connection;
       db.on('error', (err) => console.log('erorrrr', err));
     });
