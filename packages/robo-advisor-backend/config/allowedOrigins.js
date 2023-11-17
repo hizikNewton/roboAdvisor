b3cb2b258c8dc09a9afd0caf00198d886b9fa157
Bamidele Isaac <bamideleisaac11@gmail.com>
2023-11-17 16:30:54 +0100

    publish

--- packages/robo-advisor-backend/config/allowedOrigins.js
+++ packages/robo-advisor-backend/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
-    'http://localhost:3000',
-    'http://localhost:4200',
+  'http://localhost:3000',
+  'http://localhost:4200',
+  'https://robo-advisor-robo-advisor-frontend-hiziknewton.vercel.app/',
 ];
 
 module.exports = allowedOrigins;
