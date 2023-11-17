3b40c4ed5fd4a17116414ac34486aaff7e7b6693
Isaac Bamidele <isaac.bamidele@wayapaychat.com>
2023-11-17 19:18:23 +0100

    cors issue

--- packages/robo-advisor-backend/api/index.js
+++ packages/robo-advisor-backend/api/index.js
@@ -1,34 +1,35 @@
-require('dotenv').config();
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose');
-const express = require('express');
-const path = require('path');
-const corsOptions = require('../config/corsOptions');
-const connectDB = require('../config/dbConnect');
+require("dotenv").config();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const express = require("express");
+const path = require("path");
+const corsOptions = require("../config/corsOptions");
+const connectDB = require("../config/dbConnect");
 
 //app config
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 //middleware
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json()); // middleware to parse json
 app.use(cookieParser());
 
-app.use('/api', require('../routes/instrumentWeightRoute'));
+app.use("/api", require("../routes/instrumentWeightRoute"));
 
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 //db config
 connectDB();
-mongoose.connection.once('open', () => {
-  console.log('Connected to MongoDB');
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
 });
 
-mongoose.connection.on('error', (err) => {
+mongoose.connection.on("error", (err) => {
   console.log(err);
 });
 
