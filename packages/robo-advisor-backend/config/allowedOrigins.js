934732db5bada8910211485cddc22ad2fac74f34
Bamidele Isaac <bamideleisaac11@gmail.com>
2023-11-17 16:32:21 +0100

    allow cross origin

--- packages/robo-advisor-backend/config/allowedOrigins.js
+++ packages/robo-advisor-backend/config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:4200',
   'https://robo-advisor-robo-advisor-frontend-hiziknewton.vercel.app/',
+  'robo-advisor-robo-advisor-frontend.vercel.app',
 ];
 
 module.exports = allowedOrigins;
